2a70194f8797b790370e0ccc5019d545933b015e
louismeunier <louismeunier123@gmail.com>
2022-01-24 17:35:22 -0500

    Add prettier

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,33 +1,31 @@
-const localStorageKey = (key:string) => `physics-rev-${key}`;
+const localStorageKey = (key: string) => `physics-rev-${key}`;
 // clean this up
 export function setSiteThemePreference(newTheme: "dark" | "light") {
-    localStorage.setItem(
-        localStorageKey('theme'),
-        newTheme
-    );
+  localStorage.setItem(localStorageKey("theme"), newTheme);
 }
 
-export function getSiteThemePreference(): 'dark' | 'light' {
-    // @ts-ignore
-    return localStorage.getItem(localStorageKey('theme'))
+export function getSiteThemePreference(): "dark" | "light" {
+  // @ts-ignore
+  return localStorage.getItem(localStorageKey("theme"));
 }
 
 export function getSiteTheme() {
-    const app:Element = document.getElementsByTagName("body")[0];
-    return app.classList.contains("light") ? "light" : "dark";
+  const app: Element = document.getElementsByTagName("body")[0];
+  return app.classList.contains("light") ? "light" : "dark";
 }
 
 export function setSiteTheme(theme: "dark" | "light") {
-    const app:Element = document.getElementsByTagName("body")[0];
-    const oldTheme = theme == "dark" ? "light" : "dark";
-    app.classList.remove(oldTheme);
-    app.classList.add(theme);
+  const app: Element = document.getElementsByTagName("body")[0];
+  const oldTheme = theme == "dark" ? "light" : "dark";
+  app.classList.remove(oldTheme);
+  app.classList.add(theme);
 }
 
 export function toggleSiteTheme() {
-    const app:Element = document.getElementsByTagName("body")[0];
-    const [currentTheme, newTheme]: ["light" | "dark", "light" | "dark"] = app.classList.contains("light") ? ["light", "dark"] : ["dark", "light"];
-    setSiteThemePreference(newTheme);
-    app.classList.remove(currentTheme);
-    app.classList.add(newTheme);
+  const app: Element = document.getElementsByTagName("body")[0];
+  const [currentTheme, newTheme]: ["light" | "dark", "light" | "dark"] =
+    app.classList.contains("light") ? ["light", "dark"] : ["dark", "light"];
+  setSiteThemePreference(newTheme);
+  app.classList.remove(currentTheme);
+  app.classList.add(newTheme);
 }
