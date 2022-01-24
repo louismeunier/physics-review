e536885a5231b7edd8969aff7f221949bd38c758
louismeunier <louismeunier123@gmail.com>
2022-01-24 17:38:38 -0500

    Update prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import App from "./App.svelte";
-import "./assets/globals.scss";
-import "./assets/themes.scss";
+import App from './App.svelte';
+import './assets/globals.scss';
+import './assets/themes.scss';
 
 const app = new App({
-  target: document.getElementsByClassName("app")[0],
+  target: document.getElementsByClassName('app')[0],
 });
 
 export default app;
