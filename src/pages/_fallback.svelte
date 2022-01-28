39f13c4c5ef3ec382f7066c688fe8f3fac2494ff
louismeunier <louismeunier123@gmail.com>
2022-01-28 09:08:27 -0500

    Improve fallback page

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,4 +1,24 @@
+<script>
+    import Error from "../lib/components/icons/Error.svelte";
+</script>
+
 <div class="content">
     <h1>404</h1>
-    <h2>Page not found: try <a href="/">going home</a></h2>
+    <Error/>
+    <h2>Page not found!</h2>
+    <h3>Try <a href="/">going home</a>.</h3>
 </div>
+
+<style>
+    h1 {
+        color: var(--red-ish);
+        font-style: italic bold;
+    }
+    div {
+        text-align: center;
+    }
+
+    div :global(svg) {
+        width: 30%;
+    }
+</style>
