e86fc0c8cad18de6b15e2fa4a1fb261676656a03
louismeunier <louismeunier123@gmail.com>
2022-01-28 10:37:55 -0500

    Improve sidebar animations

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,23 +1,35 @@
 <script lang="ts">
     import Menu from "./icons/Menu.svelte";
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 
     let visible = false;
+    let toggleElem:HTMLElement;
+    let rotate:NodeJS.Timeout;
+    let i = 0;
 
     const toggleSidebar = () => {
+        clearInterval(rotate);
         visible = !visible;
+        rotate = setInterval(() => {
+            visible ? i += 2 : i -= 2;
+            toggleElem.style.transform = `rotate(${i}deg)`;
+            if (i % 180 == 0) {
+                clearInterval(rotate);
+            } 
+        }, 5)
     }
 </script>
 
 <div class="sidebar-wrapper">
     <div 
+        bind:this="{toggleElem}"
         class="toggle" 
         on:click="{toggleSidebar}"
     >
         <Menu/>
     </div>
     {#if visible}
-        <div class="sidebar" transition:fade="{{ duration: 300 }}">
+        <div class="sidebar" transition:fly="{{ duration: 400, x: -400 }}">
             <div>
                 <h4>item 1</h4>
             </div>
@@ -36,6 +48,7 @@
         left: 0;
         display: flex;
         width: 20%;
+        height: 100vh;
         flex-direction: column;
         align-items: baseline;
         position: absolute;
@@ -44,11 +57,12 @@
     .sidebar {
         display: flex;
         flex-direction: column;
-        width: 100%;
-        height: 100vh;
         position: absolute;
         background-color: var(--sub-color);
         left: 0;
+        bottom: 0;
+        top: 0;
+        width: 100%;
         padding-top: 45px;
         padding-left: 5px;
     }
@@ -59,8 +73,8 @@
     }
     
     .toggle {
-        position: sticky;
         z-index: 1000;
+        transform-origin: center center;
     }
 
     .toggle:hover {
