3f7730796f86a14e13c344cad22f8a5567af967a
louismeunier <louismeunier123@gmail.com>
2022-01-29 11:51:27 -0500

    Add about page

--- src/pages/about.svelte
+++ src/pages/about.svelte
@@ -1,9 +1,22 @@
 <div class="content">
   <h1>About this Site</h1>
+  <p>
+    This site is intended to be a interactive review guide for a
+    introductory-level physics course.
+  </p>
+  <p>
+    It is forever <a href="https://github.com/louismeunier" target="_blank"
+      >open source</a
+    >, ad-free, and free-to-use.
+  </p>
 </div>
 
 <style>
-  div {
+  .content {
     text-align: center;
+  }
+
+  p {
+    font-size: 1.5em;
   }
 </style>
