3d82c81de961828f6b5d3d06baca4992f300d5d7
louismeunier <louismeunier123@gmail.com>
2022-01-28 21:59:34 -0500

    Update styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,17 @@
 <div>
-    <a href='https://github.com/louismeunier' target="_blank">source code</a>
+  <a href="https://github.com/louismeunier" target="_blank">source code</a>
 </div>
 
 <style>
-    div {
-        text-align: center;
-        grid-column: 1 / 4;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        background-color: var(--sub-color);
-        gap: 5rem;
-        padding-top: 1em;
-        padding-bottom: 1em;
-    }
+  div {
+    text-align: center;
+    grid-column: 1 / 4;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    /* background-color: var(--sub-color); */
+    gap: 5rem;
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
 </style>
