c1bc255cefe067301b376bf3ff8d5544d20dcb4a
louismeunier <louismeunier123@gmail.com>
2022-01-29 17:15:23 -0500

    Re-add newton's cradle index viz

--- src/lib/components/DoublePendelum.svelte
+++ src/lib/components/DoublePendelum.svelte
@@ -1,82 +1,101 @@
 <script>
-    import Matter from 'matter-js';
-    import { onMount } from 'svelte';
+  import Matter from 'matter-js';
+  import { getSiteTheme } from '../utils/theme';
+  import { onMount } from 'svelte';
+  import { get } from 'svelte/store';
 
-    onMount(() => {
-        const {
-            Engine,
-            Events,
-            Render,
-            Runner,
-            Composite,
-            Constraint,
-            Bodies,
-            MouseConstraint,
-            Mouse,
-            Body
-        } = Matter;
+  onMount(() => {
+    const {
+      Engine,
+      Events,
+      Render,
+      Runner,
+      Composite,
+      Constraint,
+      Bodies,
+      MouseConstraint,
+      Mouse,
+      Body,
+    } = Matter;
 
+    const engine = Engine.create(),
+      world = engine.world;
 
-        const engine = Engine.create(),
-            world = engine.world;
+    const render = Render.create({
+      element: document.getElementById('double-pendelum'),
+      engine: engine,
+      options: {
+        width: (window.innerWidth * 4) / 5,
+        height: (window.innerHeight * 1) / 2,
+        background: 'transparent',
+        wireframes: false,
+      },
+    });
 
+    const newtonsCradle = Composite.create({ label: 'Newtons Cradle' });
 
-        const render = Render.create({
-            element: document.getElementById("double-pendelum"),
-            engine: engine,
-            options: {
-                width: 800,
-                height: 400,
-                background: 'transparent',
-                wireframes: false
-            }
+    const number = 5;
+    const xx = 280;
+    const yy = 50;
+    const size = 30;
+    const length = 300;
+
+    for (let i = 0; i < number; i++) {
+      const separation = 1.9,
+        circle = Bodies.circle(
+          xx + i * (size * separation),
+          yy + length,
+          size,
+          {
+            inertia: Infinity,
+            restitution: 1,
+            friction: 0,
+            frictionAir: 0,
+            slop: 1,
+            render: { fillStyle: '#2a9d8fff' },
+          }
+        ),
+        constraint = Constraint.create({
+          pointA: { x: xx + i * (size * separation), y: yy },
+          bodyB: circle,
+          render: { strokeStyle: getSiteTheme() == 'dark' ? 'white' : 'black' },
         });
 
-        const newtonsCradle = Composite.create({ label: 'Newtons Cradle' });
+      Composite.addBody(newtonsCradle, circle);
+      Composite.addConstraint(newtonsCradle, constraint);
+    }
 
-        const number = 5;
-        const xx = 280;
-        const yy = 50;
-        const size = 30;
-        const length = 300;
+    const mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false,
+          },
+        },
+      });
 
-        for (let i = 0; i < number; i++) {
-            const separation = 1.9,
-                circle = Bodies.circle(xx + i * (size * separation), yy + length, size, 
-                    { inertia: Infinity, restitution: 1, friction: 0, frictionAir: 0.0001, slop: 1, render: { fillStyle: i == 0 || i == number - 1 ? "purple" : "black" }}),
-                constraint = Constraint.create({ pointA: { x: xx + i * (size * separation), y: yy }, bodyB: circle });
+    Composite.add(world, newtonsCradle);
 
-            Composite.addBody(newtonsCradle, circle);
-            Composite.addConstraint(newtonsCradle, constraint);
-        }
+    Body.translate(newtonsCradle.bodies[0], { x: -140, y: -100 });
+    Composite.add(world, mouseConstraint);
 
-        const mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
+    render.mouse = mouse;
 
-        Composite.add(world, newtonsCradle);
+    Render.run(render);
+    const runner = Runner.create();
+    Runner.run(runner, engine);
 
-        Body.translate(newtonsCradle.bodies[0], { x: -140, y: -100 });
-        Composite.add(world, mouseConstraint);
+    Render.lookAt(render, {
+      min: { x: 0, y: 50 },
+      max: { x: 800, y: 600 },
+    });
 
-        render.mouse = mouse;
-
-        Render.run(render);
-        const runner = Runner.create();
-        Runner.run(runner, engine);
-
-        Render.lookAt(render, {
-            min: { x: 0, y: 50 },
-            max: { x: 800, y: 600 }
-        });
-    })
+    return () => {
+      Render.stop(render);
+    };
+  });
 </script>
 
-<div id="double-pendelum"/>
+<div id="double-pendelum"></div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,15 +1,15 @@
 <script>
-    import DoublePendelum from "../lib/components/DoublePendelum.svelte";
+  import DoublePendelum from '../lib/components/DoublePendelum.svelte';
 </script>
 
-<div class='content'>
-    <h1>Physics: Mechanics</h1>
-    <h2>A Review Packet</h2>
-    <!-- <DoublePendelum/> -->
+<div class="content">
+  <h1>Physics: Mechanics</h1>
+  <h2>A Review Packet</h2>
+  <DoublePendelum />
 </div>
 
 <style>
-    div {
-        text-align: center;
-    }
+  div {
+    text-align: center;
+  }
 </style>
