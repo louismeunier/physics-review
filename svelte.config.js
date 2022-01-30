b9fe137378c52f4a3b559730ea367f34a06e103f
louismeunier <louismeunier123@gmail.com>
2022-01-30 09:43:04 -0500

    Add markdown parser

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  extensions: ['.svelte', '.svx', '.md'],
+  preprocess: [mdsvex(), sveltePreprocess()],
 };
