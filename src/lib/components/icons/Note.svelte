f97df24cca076b6f6b397aaaf0f1a780030ae84e
louismeunier <louismeunier123@gmail.com>
2022-01-30 10:49:58 -0500

    Fix and add some icons

--- src/lib/components/icons/Note.svelte
+++ src/lib/components/icons/Note.svelte
@@ -3,11 +3,12 @@
     stroke-linecap="round"
     stroke-linejoin="round"
     stroke-width="2"
-    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+  ></path>
 </svg>
 
 <style>
   svg {
-    stroke: var(--red-ish);
+    stroke: var(--green);
   }
 </style>
