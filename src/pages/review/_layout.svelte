731d0aefffc874b83020bf555f8c501831974b5e
louismeunier <louismeunier123@gmail.com>
2022-02-02 07:09:09 -0500

    Add latex syntax support

---
+++ src/pages/review/_layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css"
+    integrity="sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET"
+    crossorigin="anonymous"
+  />
+</svelte:head>
+
+<div class="content">
+  <slot />
+</div>
+
+<style>
+  .content {
+    text-align: center;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import { mdsvex_config } from './mdsvex.config.js';
 
 export default {
   extensions: ['.svelte', '.svx', '.md'],
-  preprocess: [mdsvex(), sveltePreprocess()],
+  preprocess: [mdsvex(mdsvex_config), sveltePreprocess()],
 };
